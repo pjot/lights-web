c2762919c4758f773dd0d8a811c8d2c911b87e6d
Peter Bergström <petb@redpill-linpro.com>
2013-11-06 23:01:44 +0100

    fixed jquery error

--- static/lights.js
+++ static/lights.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('#green').change(function () {
         var url = '?method=ajax&light=1&action=';
-        if (this.val() == 'no')
+        if ($(this).val() == 'no')
         {
             url += 'turnOff';
         }
